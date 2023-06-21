d881d0f4754406afb7ad5c49c7bfc2a7b0564dd4
Naina Mohamed U <99238720+naina2801@users.noreply.github.com>
2023-06-21 20:00:23 +0530

    Add files via upload

---
+++ pizza_order.py
@@ -0,0 +1,21 @@
+print("welcome to python pizza delivers")
+size = input("what size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do You want perpperoni? Y or N")
+extra_cheese = input("Do you want extra cheese Y or N")
+bill = 0
+
+if size == 'S':
+    bill += 15
+elif size == 'M':
+    bill += 20
+else:
+    bill += 25
+if add_pepperoni == 'Y':
+    if size == 'S':
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == 'Y':
+    bill += 1
+print(f"your bill is{bill}")
+
